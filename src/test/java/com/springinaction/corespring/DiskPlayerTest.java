001a374e2215a27d803bcd612d710e44232772dc
Sandeep <sandeep.yadav@noosyntech.in>
2018-06-19 01:50:18 +0530

    Change the verison of junit to 4.12
    giving initialization problem when it is lower

--- src/test/java/com/springinaction/corespring/DiskPlayerTest.java
+++ src/test/java/com/springinaction/corespring/DiskPlayerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
